a645095c148c400b347b69518a1fe942c8b062b9
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-24 16:10:56 +0800

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HerosComponent } from "./heros/heros.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes', component: HerosComponent }
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HerosComponent },
 ];
 
 @NgModule({
