6e03e7679e4a54d55a191bcecf4600e7908b92d4
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-24 14:47:13 +0800

    set route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HerosComponent } from "./heros/heros.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HerosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
