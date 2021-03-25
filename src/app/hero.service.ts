fbd4d7e60a66e2b871d37d5e07abcab8776d8733
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-25 10:23:12 +0800

    add handleErroe getHero by using http method use rxjs.tap() in pipeble data to process logging

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
 
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
@@ -24,12 +25,29 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T)
+    }
+  }
+
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeros', []))
+      );
   }
-  getHero(id: number): Observable<Hero | undefined> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id || null}`);
-    return of(HEROES.find(hero => hero.id === id));
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      )
   }
 }
