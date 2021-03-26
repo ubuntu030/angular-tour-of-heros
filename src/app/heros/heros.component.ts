f47c149947c719c5aa2bac43a6713306118c4149
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-26 11:09:35 +0800

    add delete button

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -35,4 +35,9 @@ export class HerosComponent implements OnInit {
       })
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
